7f692afd149a9ac1037d129e4eb0afc43f9b077f
Lalit Kumar <lalitc211@gmail.com>
2024-02-28 15:22:18 +0530

    intern

---
+++ socialmediadashboard/socialmediadashboard-codealpha-main/resources/js/chartdoughnut.js
@@ -0,0 +1,26 @@
+const ctx = document.getElementById("doughnut");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Tik Tok", "Youtube", "Instagram"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [25000, 23000, 31000],
+        backgroundColor: ["#9077E8", "#FF9292", "#F68876"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      x: {
+        display: false,
+      },
+      y: {
+        display: false,
+      },
+    },
+  },
+});
